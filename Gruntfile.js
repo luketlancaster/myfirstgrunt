119e4372d90f7623bb5ca1ea129e64ee9ca914ac
Luke Lancaster <luketlancaster@gmail.com>
2015-02-23 15:00:04 -0600

    add gruntfile copy functionality

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.initConfig({
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'app', src: ['**'], dest: 'public/', filter: 'isFile'}
+        ]
+      }
+    }
+  });
+
+  grunt.registerTask('default', []);
+  grunt.registerTask('cp', ['copy']);
+};
