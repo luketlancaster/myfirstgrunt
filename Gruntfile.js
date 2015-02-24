725bc050bd8259754910749d71c4bfcaa49752e9
Luke Lancaster <luketlancaster@gmail.com>
2015-02-24 10:30:57 -0600

    add autoprefixing, change watch file for more specificity

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,6 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
-    watch: {
-      files: ['app/**', '!**/*_.jade'],
-      tasks: ['build'],
-      options: {
-        spawn: false,
-      },
-    },
     clean: ['public'],
     copy: {
       main: {
@@ -34,9 +27,32 @@ module.exports = function(grunt) {
           'public/css/main.css': 'app/styles/main.scss'
         }
       }
+    },
+    autoprefixer: {
+      single_file: {
+        src: 'public/**/*.css',
+        dest: 'public/css/main.css'
+      }
+    },
+    watch: {
+      other: {
+        files: ['app/**', '!app/**/*.jade', '!app/**/*.{sass,scss}'],
+        tasks: ['copy']
+      },
+      jade: {
+        files: ['app/**.jade'],
+        tasks: ['jade']
+      },
+      sass: {
+        files: ['app/**/*.{sass,scss}'],
+        tasks: ['sass']
+      }
     }
   });
 
   grunt.registerTask('default', []);
-  grunt.registerTask('build', ['clean', 'copy', 'jade', 'sass', 'watch']);
+  grunt.registerTask('build', ['clean', 'copy', 'jade', 'sass', 'autoprefixer']);
+  grunt.registerTask('serve', ['build', 'watch']);
 };
+
+
